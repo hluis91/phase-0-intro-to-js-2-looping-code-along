f7d26c42097d5a54664d93f72adc8e1bb459dade
Luis Hernandez <lshrnndz6@gmail.com>
2022-12-23 19:14:39 -0800

    commit tree 88ea076bfe49a239921ae941e4dbf50511ae4002

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+
+function writeCards(names, event){
+    let messages = [];
+    for(let i = 0; i < names.length; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    };
+    return messages;
+};
+
+// function countDown(startNumber){
+//     let numbers = []
+//     for(let count = startNumber; count >= 0; count--){
+//         console.log(numbers.push(count))
+//     }
+//     return numbers
+// }
+
+function countDown(startNumber){
+    let count = startNumber
+    while (count >= 0){
+        console.log(count--);
+    }
+    return count;
+}
+
+
+
